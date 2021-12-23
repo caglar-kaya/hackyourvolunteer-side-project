26a57917d41270e248e7ab340910089348fd9c4b
Caglar Kaya <caglar.kaya.europe@gmail.com>
2021-12-23 22:22:01 +0100

    add section counter

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@
 import { createNavbar } from './views/createNavbar.js';
 import { createSectionElder } from './views/createSectionElder.js';
 import { createSectionCards } from './views/createSectionCards.js';
+import { createSectionCounter } from './views/createSectionCounter.js';
 
 export const main = () => {
   createNavbar();
   createSectionElder();
   createSectionCards();
+  createSectionCounter();
 };
---
+++ src/views/createSectionCounter.js
@@ -0,0 +1,35 @@
+import { createDOMElement, getDOMElement } from '../helpers/DOMUtils.js';
+import { startCounter } from '../helpers/counter.js';
+
+export const createSectionCounter = () => {
+  const userInterface = getDOMElement('user-interface');
+  const sectionCounter = createDOMElement('section');
+  sectionCounter.classList.add('section-counter');
+  sectionCounter.innerHTML = `<div class="total-people">
+                                <p class="impact">The impact we make</p>
+                                <p class="community">
+                                  Our community of students has already helped 7,814 people
+                                </p>
+                                <p class="active">
+                                  We have been active throughout the Netherlands since 2009! And so
+                                  our students are called careibu's. Care-i-hows? We need to explain
+                                  that
+                                </p>
+                              </div>
+                              <div class="counters">
+                                <div class="counter-seniors">
+                                  <div class="counter" data-target="3900">0</div>
+                                  <p>Seniors from the loneliness</p>
+                                </div>
+                                <div class="counter-children">
+                                  <div class="counter" data-target="5400">0</div>
+                                  <p>Children to a babysitter</p>
+                                </div>
+                                <div class="counter-houses">
+                                  <div class="counter" data-target="1400">0</div>
+                                  <p>Clean houses</p>
+                                </div>
+                              </div>`;
+  userInterface.appendChild(sectionCounter);
+  startCounter();
+};
